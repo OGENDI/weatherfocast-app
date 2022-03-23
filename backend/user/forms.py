2c6f4c1960461e21c691fd562fb7c1d082b4a5ab
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-23 18:02:42 +0300

    specify fields to be displayed in the form

--- backend/user/forms.py
+++ backend/user/forms.py
@@ -1,4 +1,11 @@
+from dataclasses import fields
+from pyexpat import model
 from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
+    
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
+        
