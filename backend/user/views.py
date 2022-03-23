9654a9b47aac66b03bf73904b748a2e2e6f4d366
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-23 18:17:47 +0300

    import redirect,authenticate,login,logout and messages

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegisterForm
-
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
