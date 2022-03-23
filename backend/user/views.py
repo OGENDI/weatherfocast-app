25cc13a8986e7ccc49acbde5d3561cc62b77449b
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-23 18:38:12 +0300

    define built in get function for getting UserRegisterForm

--- backend/user/views.py
+++ backend/user/views.py
@@ -8,3 +8,8 @@ from django.views.generic import View
 class UserFormView(View):
     form_class=UserRegisterForm
     template_name='user/registration_form.html'
+    
+    # Get empty form
+    def get(self,request):
+        form=self.form_class(None)
+        return render(request,self.template_name,{'form':form})
