e8a4de1a15310c373b833648263790af7cdd798b
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-23 18:23:29 +0300

    specify UserRegisterForm as blueprint

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render,redirect
 from .forms import UserRegisterForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.views.generic import View
 # Create your views here.
+
+class UserFormView(View):
+    form_class=UserRegisterForm
