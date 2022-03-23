4dd8457cb1c023bcf20bfecd91fa9aa186dd8266
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-23 20:31:50 +0300

    add urls

--- backend/weatherforcast/urls.py
+++ backend/weatherforcast/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import re_path as url,include
+from django.conf import settings
+from django.conf.urls.static import static
+from user import views as user_views
+
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+     url(r'^admin/', admin.site.urls),
+     url('register/',user_views.registration,name='register'),
+     url('error/',user_views.registration,name='error'),
+     url('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+     url('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+      
+      ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
